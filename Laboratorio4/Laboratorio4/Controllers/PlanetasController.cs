a1bf7acc948ac710e7f28a1224fd51dc5b193ef1
VictorB8 <vmcb555@gmail.com>
2021-09-08 18:08:54 -0600

    Lab 4 completo

---
+++ Laboratorio4/Laboratorio4/Controllers/PlanetasController.cs
@@ -0,0 +1,85 @@
+﻿using Laboratorio4.Handlers;
+using Laboratorio4.Models;
+using System.Web.Mvc;
+
+namespace Laboratorio4.Controllers
+{
+    public class PlanetasController : Controller
+    {
+        public ActionResult listadoDePlanetas()
+        {
+            PlanetasHandler accesoDatos = new PlanetasHandler();
+            ViewBag.planetas = accesoDatos.obtenerTodoslosPlanetas();
+            return View();
+        }
+
+        public ActionResult crearPlaneta()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult crearPlaneta(PlanetaModel planeta)
+        {
+            ViewBag.ExitoAlCrear = false;
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    PlanetasHandler accesoDatos = new PlanetasHandler();
+                    ViewBag.ExitoAlCrear = accesoDatos.crearPlaneta(planeta); // recuerde que este método devuelve un booleano
+                    if (ViewBag.ExitoAlCrear)
+                    {
+                        ViewBag.Message = "El planeta" + " " + planeta.nombre + " fue creado con éxito :)";
+                        ModelState.Clear();
+                    }
+                }
+                return View();
+            }
+            catch
+            {
+                ViewBag.Message = "Algo salió mal y no fue posible crear el planeta :(";
+                return View(); // si falla se regresa a la vista original pero sin el mensaje de
+            }
+        }
+
+        [HttpGet]
+        public ActionResult editarPlaneta(int? identificador)
+        {
+            ActionResult vista;
+            try
+            {
+                PlanetasHandler accesoDatos = new PlanetasHandler();
+                PlanetaModel planetaModificar = accesoDatos.obtenerTodoslosPlanetas().Find(smodel => smodel.id == identificador);
+                if (planetaModificar == null)
+                {
+                    vista = RedirectToAction("listadoDePlanetas");
+                }
+                else
+                {
+                    vista = View(planetaModificar);
+                }
+            }
+            catch
+            {
+                vista = RedirectToAction("listadoDePlanetas");
+            }
+            return vista;
+        }
+
+        [HttpPost]
+        public ActionResult editarPlaneta(PlanetaModel planeta)
+        {
+            try
+            {
+                PlanetasHandler accesoDatos = new PlanetasHandler();
+                accesoDatos.modificarPlaneta(planeta);
+                return RedirectToAction("listadoDePlanetas", "Planetas");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Laboratorio4/Laboratorio4/Models/PlanetaModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Laboratorio4.Models
+{
+    public class PlanetaModel
+    {
+        public int id { get; set; }
+
+        [Required(ErrorMessage = "Es necesario que le indique el nombre del planeta")]
+        [Display(Name = "Ingrese el nombre del planeta")]
+        public string nombre { get; set; }
+
+        [Required(ErrorMessage = "Es necesario que le indique el tipo del planeta")]
+        [Display(Name = "Seleccione el tipo de planeta")]
+        public string tipo { get; set; }
+
+        [Required(ErrorMessage = "Es necesario que ingrese el numero de anillos")]
+        [Display(Name = "Ingrese el numero de anillos del planeta")]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Debe ingresar numeros")]
+        public int numeroAnillos { get; set; }
+
+        [Required(ErrorMessage = "Debe agregar un archivo (imagen, pdf, etc...)")]
+        [Display(Name = "Ingrese el archivo con los detalles del planeta")]
+        public HttpPostedFileBase archivo { get; set; }
+
+        public string tipoArchivo { get; set; }
+        // de momento no se va trabajar con el archivo...
+    }
+}
